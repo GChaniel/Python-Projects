024e866cf9fd50b9655e36a65a2422ec2319164f
Isaac <Isaacgchaniel@icloud.com>
2026-09-19 23:45:11 +0100

    Created class inheritance for vehicle

---
+++ inheritance_exercise.py
@@ -0,0 +1,15 @@
+
+
+class Vehicle:
+    brand = 'Toyota'
+    model = ' '
+    year = '2025'
+
+class Car(Vehicle):
+    engine_size = ''
+    petrol_type = ''
+
+class Motorcycle(Vehicle):
+    engine_size = ''
+    Petrol_type = ''
+    
